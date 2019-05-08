e526a1f812f3a3b6e83c6612b4e8690c63dcd32b
RyanCho <mith1004@gmail.com>
2019-05-08 23:16:17 +0900

    add animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
     <div>
       <div className="Hero">
         <div className="HeroGroup">
-          <h1>Learn to design and code React apps</h1>
+          <h1>Learn to <br/>design and code React apps</h1>
           <p>Complete coursed about the best tools and design systems. Prototype and build apps with React and Swift</p>
           <Link to="/video">Watch the video</Link>
         </div>
