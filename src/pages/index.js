6fd79cc6d07488b755abcb2812b5921c8ab76051
RyanCho <mith1004@gmail.com>
2019-05-09 00:01:22 +0900

    add cardGroup and card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from '../components/layout'
+import Card from "../components/Card"
 
 const IndexPage = () => (
   <Layout>
@@ -34,6 +35,15 @@ const IndexPage = () => (
               />
             </path>
           </svg>
+        </div>
+      </div>
+      <div className={"Cards"}>
+        <h2>11 courses, more coming</h2>
+        <div className={"CardGroup"}>
+          <Card title={"Design System"} text={"10 sections"} image={require('../images/wallpaper.jpg')}/>
+          <Card title={"React for Designers"} text={"12 sections"} image={require('../images/wallpaper2.jpg')}/>
+          <Card title={"Sound Desing"} text={"5 sections"} image={require('../images/wallpaper3.jpg')}/>
+          <Card title={"ARKit 2"} text={"10 sections"} image={require('../images/wallpaper4.jpg')}/>
         </div>
       </div>
     </div>
