5345f250952706ec25de6a13e50691a86aaa55ba
RyanCho <mith1004@gmail.com>
2019-05-08 21:13:48 +0900

    add css

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,7 @@ import { StaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => (
+const Layout = ({ children, data }) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
@@ -26,21 +26,7 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        {children()}
       </>
     )}
   />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Learn to <br />design and code React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code</h1>
+        <p>Complete coursed about the best tools and design systems. Prototype and build apps with React and Swift</p>
+        <Link to="/video">Watch the video</Link>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+
+  </div>
 )
 
 export default IndexPage
