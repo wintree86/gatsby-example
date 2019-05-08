d106bc60390b5527d8992869791416017cfec690
RyanCho <mith1004@gmail.com>
2019-05-09 08:01:26 +0900

    add scroll event in Header Component

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,45 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 import './Header.css';
 
-const Header = ({ siteTitle }) => (
-  <div className={"Header"}>
-    <div className={"HeaderGroup"}>
-      <Link to={"/"}><img src={require('../images/logo-designcode.svg')} width={"30"}/></Link>
-      <Link to={"/courses"}>Courses</Link>
-      <Link to={"/downloads"}>Downloads</Link>
-      <Link to={"/workshops"}>Workshps</Link>
-      <Link to={"/buy"}><button>Buy</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasScrolled: false
+    }
+  }
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
 
-Header.defaultProps = {
-  siteTitle: ``,
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset;
+    if (scrollTop > 50) {
+      this.setState({
+        hasScrolled: true
+      })
+    } else {
+      this.setState({
+        hasScrolled: false
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className={"HeaderGroup"}>
+          <Link to={"/"}><img src={require('../images/logo-designcode.svg')} width={"30"}/></Link>
+          <Link to={"/courses"}>Courses</Link>
+          <Link to={"/downloads"}>Downloads</Link>
+          <Link to={"/workshops"}>Workshps</Link>
+          <Link to={"/buy"}><button>Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header
